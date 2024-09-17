45a93ad51964335e823fdedd5e502b5b85954605
challavinaykumar <cvinaykumar82gmail.com>
2024-09-17 12:29:42 +0530

    final

--- src/Main/Home.js
+++ src/Main/Home.js
@@ -1,21 +1,24 @@
 /** @format */
 
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 // import Nav from '../Components/Nav';
 import "./Home.css";
 import Homefooter from "./Homefooter";
 import Rating from "./Dropdown/Rating";
 import Insurance from "./Insurance";
-import Asian from "../Images/AsianVascularHospitals.jpg"
-import praveen from "../Images/Dr G V Praveen Kumar.jpg"
+import Asian from "../Images/AsianVascularHospitals.jpg";
+import praveen from "../Images/Dr G V Praveen Kumar.jpg";
 import { Link } from "react-router-dom";
+import Avhservice1 from "../Images/AVH_Website_Services-01.png"
+import Avhservice2 from "../Images/AVH_Website_Services-02.png"
+
 // import narayana from '../Images/narayana.jpeg'
 
 // import praveen_kumar from '../Images/Our_doctors/Dr. G.V. Praveen Kumar.png'
 
-import praveen_kumar from '../Images/Doctors_new_images/Dr. G.V. Praveen Kumar.png'
-import nirupama_devi from '../Images/Doctors_new_images/Dr. A. Nirupama Devi.png'
-import laxmi_narayana from '../Images/Doctors_new_images/Dr. A. Laxminarayana.png'
+import praveen_kumar from "../Images/Doctors_new_images/Dr. G.V. Praveen Kumar.png";
+import nirupama_devi from "../Images/Doctors_new_images/Dr. A. Nirupama Devi.png";
+import laxmi_narayana from "../Images/Doctors_new_images/Dr. A. Laxminarayana.png";
 
 // import Header from '../Components/Header';
 // import { Link } from 'react-router-dom';
@@ -23,7 +26,7 @@ import laxmi_narayana from '../Images/Doctors_new_images/Dr. A. Laxminarayana.pn
 const Home = () => {
   // const read_more = useRef()
   // const read_btn = useRef()
-  
+
   // const read_click = ()=>{
   //   read_more.current.style.display = 'block'
   //   read_btn.current.style.display = 'none'
@@ -37,7 +40,7 @@ const Home = () => {
       <section className="home">
         {/* <img src="https://img.freepik.com/free-photo/young-handsome-physician-medical-robe-with-stethoscope_1303-17820.jpg?t=st=1723808687~exp=1723812287~hmac=fde4a9c5430cea9bec263e2b167099db0d0ec8005ea6f923dda187e4ab870dcf&w=996" alt="" /> */}
         <div className="home_txt">
-          <h1 >
+          <h1>
             Leading Specialists in <br />
             <span>Vascualr Treatments </span>
             <br /> in india
@@ -52,26 +55,30 @@ const Home = () => {
           <div className="abt_txt" data-aos="fade-right">
             <p>About</p>
             <div className="abt_txt_head">
-              <h1>Why <br/>Asian Vascular</h1>
+              <h1>
+                Why <br />
+                Asian Vascular
+              </h1>
               <h1>Hospitals?</h1>
             </div>
           </div>
           <div className="abt_image_main d-flex gap-5 align-items-center">
             <div className="abt_image" data-aos="fade-up">
-              <img
-                className="abt_img"
-                src={Asian}
-                alt=""
-              />
+              <img className="abt_img" src={Asian} alt="" />
               <div className="" data-aos="fade-up">
                 <p className="circle_txt">High quality care</p>
               </div>
             </div>
             <div className="abt_lrm" data-aos="fade-up">
-              <p style={{textAlign:'justify'}}>
-              We are distinctive from a regular commercial hospital, we've been treating the patients with the utmost care and precision manoeuvred over <b>20,000</b> successful surgeries since our short emergence and nursing the Patients back to their confident healthy selves. 
-Apart from regular surgeries, we heaved complicated cases which kept defying the odds persistently where other hospitals have given up on the patients significantly impacting their golden lives.
-
+              <p style={{ textAlign: "justify" }}>
+                We are distinctive from a regular commercial hospital, we've
+                been treating the patients with the utmost care and precision
+                manoeuvred over <b>20,000</b> successful surgeries since our
+                short emergence and nursing the Patients back to their confident
+                healthy selves. Apart from regular surgeries, we heaved
+                complicated cases which kept defying the odds persistently where
+                other hospitals have given up on the patients significantly
+                impacting their golden lives.
               </p>
               {/* <p style={{textAlign:'justify'}} className="read_hide" ref={read_more}>We are distinctive from a regular commercial hospital, we've been treating the patients with the utmost care and precision manoeuvred over 20,000 successful surgeries since our short emergence and nursing the Patients back to their confident healthy selves. 
 Apart from regular surgeries, we heaved complicated cases which kept defying the odds persistently where other hospitals have given up on the patients significantly impacting their golden lives.
@@ -90,7 +97,7 @@ Apart from regular surgeries, we heaved complicated cases which kept defying the
             <div className="ser_sub d-flex gap-5 align-items-center">
               <div className="ser_images ser_images_1" data-aos="fade-right">
                 <img
-                  src="https://ysm-res.cloudinary.com/image/upload/ar_1:1,c_fill,dpr_3.0,f_auto,g_faces:auto,q_auto:eco,w_500/v1/yms/prod/4760f4d2-d982-40b7-a339-3af67e354105"
+                src={Avhservice1}
                   alt=""
                 />
                 <h1>Vascular</h1>
@@ -98,13 +105,15 @@ Apart from regular surgeries, we heaved complicated cases which kept defying the
               <div className="ser_txt " data-aos="fade-up">
                 <p>Services</p>
                 <h2 className="hm_ser">
-                At Asian Vascular Hospitals, excel would belittle our Mastership in providing a Pinnacle of Healthcare in these critical Vascular and Cardiovascular Departments.
+                  At Asian Vascular Hospitals, excel would belittle our
+                  Mastership in providing a Pinnacle of Healthcare in these
+                  critical Vascular and Cardiovascular Departments.
                 </h2>
               </div>
             </div>
             <div className="ser_images ser_images_2" data-aos="fade-up">
               <img
-                src="https://c-care.com/mu/wp-content/uploads/sites/2/2022/05/Cardiology-top-paragraph.jpg"
+              src={Avhservice2}
                 alt=""
               />
               <h1>Cardio Vascular</h1>
@@ -112,8 +121,11 @@ Apart from regular surgeries, we heaved complicated cases which kept defying the
           </div>
         </div>
       </section>
-     
-      <section className="asian_vas" style={{display:"flex",justifyContent:"center"}}>
+
+      <section
+        className="asian_vas"
+        style={{ display: "flex", justifyContent: "center" }}
+      >
         <div className="d-flex about_main abt_main_2 gap-4">
           <div className="abt_txt abt_txt_2">
             {/* <p>About</p> */}
@@ -135,7 +147,7 @@ Apart from regular surgeries, we heaved complicated cases which kept defying the
             </div>
             <div className="abt_lrm" data-aos="fade-up">
               {/* <img src={cathLab} className="abt_img" style={{width:"250px",height:"200px"}}> */}
-               
+
               {/* </img> */}
               {/* <small>READ MORE <i class="fa-solid fa-angle-right"></i></small> */}
             </div>
@@ -144,57 +156,61 @@ Apart from regular surgeries, we heaved complicated cases which kept defying the
       </section>
 
       <div className="doctors_float">
-      <section className="doctors" data-aos="fade-up">
-        <div className=" dox_sec text-center">
-          <div className="doctors_head mb-5" data-aos="fade-up">
-            <p>Оur Doctors</p>
-            <h1>Meet Our Specialists</h1>
-            {/* <Link to={'/Ourdoctors'}><span className="our_doctors_more">View More <i class="fa-solid fa-arrow-right"></i> </span></Link> */}
+        <section className="doctors" data-aos="fade-up">
+          <div className=" dox_sec text-center">
+            <div className="doctors_head mb-5" data-aos="fade-up">
+              <p>Оur Doctors</p>
+              <h1>Meet Our Specialists</h1>
+              {/* <Link to={'/Ourdoctors'}><span className="our_doctors_more">View More <i class="fa-solid fa-arrow-right"></i> </span></Link> */}
+            </div>
+            <div className="doc_main d-flex justify-content-center">
+              <div
+                className="doctors_images doctors_images_1"
+                data-aos="zoom-in"
+              >
+                <img src={praveen_kumar} alt="" />
+                <div className="doc_txt">
+                  <h1 className="fs-3">Dr. G.V. Praveen Kumar</h1>
+                  <p>Founder Chairmen & Chief Consultant</p>
+                  <p>Vascular & Endovascular Surgeon</p>
+                  <p>MS, DNB (Vascular Surgery)</p>
+                </div>
+              </div>
+
+              <div
+                className="doctors_images doctors_images_2"
+                data-aos="zoom-in"
+              >
+                <img src={nirupama_devi} alt="" />
+                <div className="doc_txt doc_txt_2">
+                  <h1 className="fs-3">Dr. A. Nirupama Devi</h1>
+                  <p> Managing Director & Consultant Pulmonologist</p>
+                  <p> MBBS, DTCD</p>
+                </div>
+              </div>
+
+              <div
+                className="doctors_images doctors_images_3"
+                data-aos="zoom-in"
+              >
+                <img src={laxmi_narayana} alt="" />
+                <div className="doc_txt">
+                  <h1 className="fs-3">Dr. A. Laxminarayana</h1>
+                  <p>Cardio Thoracic Vascular Surgeon</p>
+                  <p style={{ paddingBottom: "20px" }}>MBBS, M.CH</p>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="doc_main d-flex justify-content-center">
-            <div className="doctors_images doctors_images_1" data-aos="zoom-in">
-              <img
-                src={praveen_kumar}
-                alt=""
-              />
-              <div className="doc_txt">
-                <h1 className="fs-3">Dr. G.V. Praveen Kumar</h1>
-                <p>Founder Chairmen & Chief Consultant</p>
-                <p>Vascular & Endovascular Surgeon</p>
-                <p>MS, DNB (Vascular Surgery)</p>
-              </div>
-            </div>
-
-            <div className="doctors_images doctors_images_2" data-aos="zoom-in">
-              <img
-                src={nirupama_devi}
-                alt=""
-              />
-              <div className="doc_txt doc_txt_2">
-                <h1 className="fs-3">Dr. A. Nirupama Devi</h1>
-                <p> Managing Director & Consultant Pulmonologist</p>
-                <p> MBBS, DTCD</p>
-              </div>
-            </div>
-
-            <div className="doctors_images doctors_images_3" data-aos="zoom-in">
-              <img
-                src={laxmi_narayana}
-                alt=""
-              />
-              <div className="doc_txt">
-                <h1 className="fs-3">Dr. A. Laxminarayana</h1>
-                <p >Cardio Thoracic Vascular Surgeon</p>
-                <p style={{paddingBottom:'20px'}}>MBBS, M.CH</p>
-              </div>
-            </div>
-          </div>
-
+        </section>
+        <div className="view_more_btn">
+          <Link to={"/Ourdoctors"}>
+            <span className="our_doctors_more">
+              View More<small>Doctors</small>
+              <i class="fa-solid fa-arrow-right"></i>{" "}
+            </span>
+          </Link>
         </div>
-      </section>
-      <div className="view_more_btn">
-        <Link to={'/Ourdoctors'}><span className="our_doctors_more">View More<small>Doctors</small><i class="fa-solid fa-arrow-right"></i> </span></Link>
-      </div>
       </div>
 
       <section className="concern" data-aos="fade-up">
@@ -203,30 +219,28 @@ Apart from regular surgeries, we heaved complicated cases which kept defying the
             {/* <p>About</p> */}
             <div className="abt_txt_head abt_txt_head_3">
               {/* <h1>Whatever </h1> */}
-              <h1>Your Concern, <br/> Our <br />Mastery!</h1>
+              <h1>
+                Your Concern, <br /> Our <br />
+                Mastery!
+              </h1>
             </div>
           </div>
           <div className="abt_image_main d-flex gap-5 align-items-center">
             <div className="abt_image abt_image_3" data-aos="fade-right">
-              <img
-                className="abt_img"
-                src={praveen}
-                alt=""
-              />
+              <img className="abt_img" src={praveen} alt="" />
               {/* <p className='circle_txt'>High quality care</p> */}
             </div>
             <div className="abt_lrm abt_lrm_3" data-aos="fade-up">
               <p>
-              We stand by our word when we proclaim our proficiency in handling over <b> 20,000</b> successful surgeries comprising various complications, concerns and a broad spectrum of rejected cases of other prominent hospitals.
+                We stand by our word when we proclaim our proficiency in
+                handling over <b> 20,000</b> successful surgeries comprising
+                various complications, concerns and a broad spectrum of rejected
+                cases of other prominent hospitals.
               </p>
 
               <ul>
-                <li>
-                Varicose Veins
-                </li>
-                <li>
-                Deep Vein Thrombosis (DVT)
-                </li>
+                <li>Varicose Veins</li>
+                <li>Deep Vein Thrombosis (DVT)</li>
                 <li>Aneurysms</li>
                 <li> Peripheral Neuropathy</li>
                 <li>Vascular Malformations</li>
@@ -241,11 +255,11 @@ Apart from regular surgeries, we heaved complicated cases which kept defying the
       </section>
 
       <div className="home_rating">
-        <Rating/>
+        <Rating />
       </div>
 
       <div className="">
-        <Insurance/>
+        <Insurance />
       </div>
 
       {/* <div className="endo_rating reno_rating">
